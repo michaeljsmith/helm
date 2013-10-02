7e69b2223287f14af3bde7535dd47e6f86afd3d7
Michael Smith <msmith@msmith.id.au>
2013-10-02 17:33:40 +1000

    quadtree progress

--- src/quadtree.h
+++ src/quadtree.h
@@ -11,11 +11,22 @@ class QuadTree {
   using Value = T;
 
   struct Area {
+    Area(Coord _left, Coord _top, Coord _right, Coord _bottom):
+      left(_left), top(_top), right(_right), bottom(_bottom) {}
+
+    Coord left;
+    Coord top;
+    Coord right;
+    Coord bottom;
   };
 
   template <typename Visitor>
-  void visit(Area const& area, Visitor&& visitor) {
-    VisitRecurser<Visitor>(area, visitor)(root, rootScale(), rootOffsetX(), rootOffsetY());
+  void visit(Area const& area, Visitor&& visitor) const {
+    VisitRecurser<Visitor>(area, visitor)(root, rootScaleExponent, rootOffsetIndexX, rootOffsetIndexY);
+  }
+
+  void insert(Area const& area, Value payload) {
+    asdf();
   }
 
  private:
@@ -25,6 +36,9 @@ class QuadTree {
   };
 
   shared_ptr<Node> root;
+  int rootScaleExponent;
+  int rootOffsetIndexX;
+  int rootOffsetIndexY;
 
   template <typename Visitor>
   struct VisitRecurser {
@@ -34,8 +48,44 @@ class QuadTree {
     VisitRecurser(Area const& _area, Visitor& _visitor): visitor(_visitor), area(_area) {
     }
 
-    void visitRecurse(shared_ptr<Node> const& node, Coord scale, Coord x, Coord y) {
-      asdf();
+    void visitRecurse(shared_ptr<Node> const& node, int scaleExponent, int offsetIndexX, int offsetIndexY) {
+      if (node == nullptr) {
+        return;
+      }
+
+      auto strictSize = Coord(1 << scaleExponent);
+      auto left = offsetIndexX * strictSize;
+      auto top = offsetIndexY * strictSize;
+
+      auto right = left + strictSize * 2;
+      auto bottom = top + strictSize * 2;
+
+      if (area.right < left || area.left >= right) {
+        return;
+      }
+
+      if (area.bottom < top || area.top >= bottom) {
+        return;
+      }
+
+      for (auto item : node->items) {
+        auto& bounds = item.bounds;
+
+        if (area.right < bounds.left || area.left >= bounds.right) {
+          continue;
+        }
+
+        if (area.bottom < bounds.top || area.top >= bounds.bottom) {
+          continue;
+        }
+
+        visitor(bounds, item.value);
+      }
+
+      visitRecurse(node->children[0][0], scaleExponent - 1, 2 * offsetIndexX, 2 * offsetIndexY);
+      visitRecurse(node->children[0][1], scaleExponent - 1, 2 * offsetIndexX, 2 * offsetIndexY + 1);
+      visitRecurse(node->children[1][0], scaleExponent - 1, 2 * offsetIndexX + 1, 2 * offsetIndexY);
+      visitRecurse(node->children[1][1], scaleExponent - 1, 2 * offsetIndexX + 1, 2 * offsetIndexY + 1);
     }
   };
 
@@ -60,9 +110,10 @@ inline void testQuadTree() {
 
   auto queryAsSet = [](Tree const& tree, Area const& area) {
     set<string> results;
-    tree.visit(area, [&results] (Area const& area, string const& payload) {
+    tree.visit(area, [&results] (Area const& /*area*/, string const& payload) {
       results.insert(payload);
     });
+    return results;
   };
 
   {
